e3aac0e0b1cb08aca52241c914c6323af1ab3d1b
Benedikt Mandelkow <benedikt.mandelkow@rwth-aachen.de>
2020-11-01 09:49:44 +0100

    switch to imgui-wgpu simple_api

--- implot-examples/implot-wgpu-demo/src/main.rs
+++ implot-examples/implot-wgpu-demo/src/main.rs
@@ -1,34 +1,60 @@
-use examples_shared;
 use imgui::{im_str, Condition, Window};
+use imgui_wgpu::simple_api;
 use implot::Context;
 
-// the actual implot samples are in there TODO(4bb4) move to using examples-shared instead
-mod support;
+struct State {
+    height: f32,
+    width: f32,
+    highdpi_factor: f64,
+    plotcontext: implot::Context,
+
+    showing_demo: bool,
+    showing_rust_demo: bool,
+}
 
 fn main() {
-    let system = support::init(file!());
-    let mut showing_demo = false;
-    let mut showing_rust_demo = true;
-    let plotcontext = Context::create();
-    system.main_loop(move |_, ui| {
-        // The context is moved into the closure after creation so plot_ui is valid.
-        let plot_ui = plotcontext.get_plot_ui();
+    let config = simple_api::Config {
+        on_resize: &|input, state: &mut State, hdpi| {
+            state.height = input.height as f32;
+            state.width = input.width as f32;
+            state.highdpi_factor = hdpi;
+        },
+        ..Default::default()
+    };
 
-        if showing_demo {
-            implot::show_demo_window(&mut showing_demo);
+    let plotcontext = Context::create();
+
+    let state = State {
+        height: 100.0,
+        width: 100.0,
+        highdpi_factor: 2.0,
+        plotcontext,
+
+        showing_demo: false,
+        showing_rust_demo: true,
+    };
+
+    imgui_wgpu::simple_api::run(config, state, |ui, state| {
+        let plot_ui = state.plotcontext.get_plot_ui();
+
+        if state.showing_demo {
+            implot::show_demo_window(&mut state.showing_demo);
         }
 
-        if showing_rust_demo {
+        if state.showing_rust_demo {
             examples_shared::show_demos(ui, &plot_ui);
         }
 
         Window::new(im_str!("Welcome to the ImPlot-rs demo!"))
             .size([430.0, 450.0], Condition::FirstUseEver)
             .build(ui, || {
-                ui.checkbox(im_str!("Show C++ ImPlot demo window"), &mut showing_demo);
+                ui.checkbox(
+                    im_str!("Show C++ ImPlot demo window"),
+                    &mut state.showing_demo,
+                );
                 ui.checkbox(
                     im_str!("Show Rust ImPlot demo windows"),
-                    &mut showing_rust_demo,
+                    &mut state.showing_rust_demo,
                 );
                 // TODO(4bb4) ... move windows by default so this is less confusing
                 ui.text_wrapped(im_str!(
